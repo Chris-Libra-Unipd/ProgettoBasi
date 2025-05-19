719393e296c8c28f919af0ea6f8fb342563727b4
D-lore-unipd <root@LaptopDavide.>
2025-05-19 17:21:42 +0200

    peisrnbgpienqri'ng

--- create.sql
+++ create.sql
@@ -1,106 +1,154 @@
+DROP TABLE  IF EXISTS Creazione;
+DROP TABLE  IF EXISTS Conoscenza;
 DROP TABLE  IF EXISTS Ingresso_Libero;
 DROP TABLE  IF EXISTS Ingresso_Guidato;
-DROP TABLE  IF EXISTS Biglietti;
-DROP TABLE  IF EXISTS Visite_Guidate;
-DROP TABLE  IF EXISTS Esposizioni;
-DROP TABLE  IF EXISTS Argomenti;
-DROP TABLE  IF EXISTS Guide;
-DROP TABLE  IF EXISTS Dipendenti;
-DROP TABLE  IF EXISTS Aree;
-DROP TABLE  IF EXISTS Autori;
+DROP TABLE  IF EXISTS Biglietto;
+DROP TABLE  IF EXISTS Visita_Guidata;
+DROP TABLE  IF EXISTS Artefatto;
+DROP TABLE  IF EXISTS Esposizione;
+DROP TABLE  IF EXISTS Argomento;
+DROP TABLE  IF EXISTS Guida;
+DROP TABLE  IF EXISTS Dipendente;
+DROP TABLE  IF EXISTS Area;
+DROP TABLE  IF EXISTS Artista;
 
-DROP TYPE  IF EXISTS Turno;
+DROP TYPE  IF EXISTS Turni_Giornata;
 DROP TYPE  IF EXISTS Tipo;
 
 
-CREATE TYPE Turno AS ENUM (
+CREATE TYPE Turni_Giornata AS ENUM (
     'Mattina',
     'Pomeriggio'
 );
-CREATE TYPE tipo AS ENUM (
+CREATE TYPE Tipo AS ENUM (
     'Opera',
     'Reperto'
 );
 
 
-CREATE TABLE Autori(
+CREATE TABLE Artista(
     Nome VARCHAR(32) NOT NULL, 
     Cognome VARCHAR(32) NOT NULL,
-    Data_Nascita DATE NOT NULL,
-    Data_Morte DATE NULL,
-    PRIMARY KEY (Nome,Cognome,Data_Nascita)
+    DN DATE NOT NULL,
+    DM DATE NULL,
+    PRIMARY KEY (Nome,Cognome,DN)
 
 );
 
-CREATE TABLE Aree(
+CREATE TABLE Area(
     Nome VARCHAR(16) PRIMARY KEY
 );
 
-CREATE TABLE Dipendenti(
+CREATE TABLE Dipendente(
     CF CHAR(16) PRIMARY KEY
 );
 
-CREATE TABLE Guide(
+CREATE TABLE Guida(
     CF CHAR(16) PRIMARY KEY,
-    FOREIGN KEY (CF) REFERENCES Dipendenti(CF)
+    FOREIGN KEY (CF) REFERENCES Dipendente(CF)
         ON DELETE NO ACTION 
         ON UPDATE CASCADE
 );
 
-CREATE TABLE Argomenti(
+CREATE TABLE Argomento(
     Nome VARCHAR(16) PRIMARY KEY
 );
 
-CREATE TABLE Esposizioni(
-    Area VARCHAR(16) NOT NULL,
+CREATE TABLE Esposizione(
+    Area VARCHAR(16) PRIMARY KEY,
     Nome VARCHAR(16) NOT NULL,
-    FOREIGN KEY (Area) REFERENCES Aree(Nome)
+    FOREIGN KEY (Area) REFERENCES Area(Nome)
         ON DELETE NO ACTION 
         ON UPDATE CASCADE,
-    PRIMARY KEY (Area)
 );
 
-CREATE TABLE Visite_Guidate(
-    Numero_Partecipanti INT NOT NULL CHECK (Numero_Partecipanti>=0),
-    Data_Visita DATE NOT NULL,
-    Turno_Visita Turno NOT NULL,
+CREATE TABLE Artefatto(
+    CHAR(4) Codice PRIMARY KEY,
+    Nome VARCHAR(32) NOT NULL, 
+    Tipologia Tipo NOT NULL, 
+    Esposizione VARCHAR(16) NULL,
+    FOREIGN KEY (Esposizione) REFERENCES Esposizione(Area)
+        ON DELETE NO ACTION 
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE Visita_Guidata(
+    Esposizione VARCHAR(16) NOT NULL,
     Guida CHAR(16) NOT NULL,
-    Area VARCHAR(16) NOT NULL,
-    FOREIGN KEY (Guida) REFERENCES Guide(CF)
+    Data_Visita DATE NOT NULL,
+    Turno Turni_Giornata NOT NULL,
+    Partecipanti INT NOT NULL CHECK (Partecipanti>=0),
+    PRIMARY KEY (Data_Visita,Turno_Visita,Area,Guida),
+
+    FOREIGN KEY (Guida) REFERENCES Guida(CF)
         ON DELETE NO ACTION
         ON UPDATE CASCADE ,
-    FOREIGN KEY (Area) REFERENCES Esposizioni(Area)
+    
+    FOREIGN KEY (Esposizione) REFERENCES Esposizione(Area)
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE Biglietto (
+    ID INT NOT NULL CHECK (ID>=0) PRIMARY KEY,
+    Data_Acq DATE NOT NULL,
+    EMail VARCHAR(32)
+);
+
+
+
+CREATE TABLE Ingresso_Guidato(
+    ID INT NOT NULL CHECK (ID>=0) PRIMARY KEY,
+    Data_Visita DATE NOT NULL,
+    Turno Turni_Giornata  NOT NULL,
+    Guida CHAR(16) NOT NULL,
+    Esposizione VARCHAR(16) NOT NULL,
+    Data_Inizio DATE NOT NULL,
+    FOREIGN KEY (Data_Visita,Turno_Visita,Area,Guida)REFERENCES Visita_Guidata(Data_Visita,Turno,Esposizione,Guida)
         ON DELETE NO ACTION
         ON UPDATE CASCADE,
-    PRIMARY KEY (Data_Visita,Turno_Visita,Area,Guida)
+    FOREIGN KEY (ID) REFERENCES Biglietto(ID)
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE
 );
-
-CREATE TABLE Biglietti (
-    ID SERIAL PRIMARY KEY,
-    Data_Acquisto DATE NOT NULL,
-    Mail VARCHAR(32)
-);
-
 CREATE TABLE Ingresso_Libero(
-    ID SERIAL PRIMARY KEY,
-    FOREIGN KEY (ID) REFERENCES Biglietti(ID)
+    ID INT NOT NULL CHECK (ID>=0) PRIMARY KEY,
+    FOREIGN KEY (ID) REFERENCES Biglietto(ID)
         ON DELETE NO ACTION
         ON UPDATE CASCADE,
     Data_Validita DATE NOT NULL
 );
 
 
-CREATE TABLE Ingresso_Guidato(
-    ID SERIAL PRIMARY KEY,
-    Data_Visita DATE NOT NULL,
-    Turno_Visita Turno  NOT NULL,
+
+CREATE TABLE Conoscenza(
+    Argomento VARCHAR(16) NOT NULL,
     Guida CHAR(16) NOT NULL,
-    Area VARCHAR(16) NOT NULL,
-    Data_Inizio DATE NOT NULL,
-    FOREIGN KEY (Data_Visita,Turno_Visita,Area,Guida)REFERENCES Visite_Guidate(Data_Visita,Turno_Visita,Area,Guida)
+    PRIMARY KEY (Argomento,Guida),
+    FOREIGN KEY (Argomento) REFERENCES Argomento(Nome)
         ON DELETE NO ACTION
-        ON UPDATE CASCADE,
-    FOREIGN KEY (ID) REFERENCES Biglietti(ID)
+        ON UPDATE CASCADE ,    
+    FOREIGN KEY (Guida) REFERENCES Guida(CF)
         ON DELETE NO ACTION
         ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE Creazione(
+    CHAR(4) Codice NOT NULL,
+    Nome VARCHAR(32) NOT NULL, 
+    Cognome VARCHAR(32) NOT NULL,
+    DN DATE NOT NULL,
+    PRIMARY Key(Codice, Nome, Cognome, DN)
+    
+    FOREIGN KEY (Codice) REFERENCES Artefatto(Codice)
+    ON DELETE NO ACTION 
+    ON UPDATE CASCADE,
+    
+    FOREIGN KEY (Nome, Cognome, DN) REFERENCES Artista(Nome, Cognome, DN)
+    ON DELETE NO ACTION 
+    ON UPDATE CASCADE
+
 );
